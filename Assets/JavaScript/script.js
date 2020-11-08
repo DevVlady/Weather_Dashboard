7b65e0060693940acc418b966f5cf5f164e6d358
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-08 11:42:44 -0600

    Created the onlick function for the search option on the webpage.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,6 +1,14 @@
 //Document ready function
 $(document).ready(function (){
 
+    $("#search-button").on("click", function(){
+        //Search value variable
+        var searchValue = $("#search-value").val();
+        //Pulling the search with a value
+        $("#search-value").val("");
+        //Calling the SearchWeather function with a searchValue
+        searchWeather(searchValue);
+    })
 
 
 
@@ -17,15 +25,4 @@ $(document).ready(function (){
 
 
 
-
-
-
-
-
-
-
-
-
-
-    
 })
