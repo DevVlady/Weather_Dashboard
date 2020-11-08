8ccf5bc98b768f852de49f908d3d26adaef312d7
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-08 11:49:22 -0600

    Added the ajax function to use the API for the weather. Also added the function to empty out the data once a new search is done.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -10,6 +10,23 @@ $(document).ready(function (){
         searchWeather(searchValue);
     })
 
+    //Function to pull in date when info is put into the search input field
+    function searchWeather (searchValue) {
+        //Ajax used to obtain the weather api
+        $.ajax({
+            type: "GET",
+            url: `   ${searchValue}    `,
+            dataType: "json",
+        }).then(function(data){
+            //History link for the search (.push)
+
+            //Local storage set item
+
+            //Used to empty out the data after every search so its not dumped on top of one another
+            $("#today").empty();
+        })
+    }
+
 
 
 
