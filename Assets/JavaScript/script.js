60ff4136a56f3e6c04c9a8588235b40a1c01dd74
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-08 12:13:11 -0600

    Added variables that i will then append to the card in html file.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -15,7 +15,7 @@ $(document).ready(function (){
         //Ajax used to obtain the weather api
         $.ajax({
             type: "GET",
-            url: `   ${searchValue}    `,
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=8e7b50e448721200d703d303ab7dc6a5`,
             dataType: "json",
         }).then(function(data){
             //History link for the search (.push)
@@ -24,6 +24,18 @@ $(document).ready(function (){
 
             //Used to empty out the data after every search so its not dumped on top of one another
             $("#today").empty();
+            //Created a card where the weather info will be appended to in the HTML file
+            var title = $("<h3>").addClass("card-title").text(_______);
+            var card = $("<div>").addClass("card");
+            var wind = $("<p>").addClass("card-text").text(`Wind Speed: ${_______}`);
+            var humid = $("<p>").addClass("card-text").text(`Humidity: ${_________}`);
+
+            var cardBody = $("<div>").addClass("card-body");
+
+            //Appending all variables/tags created above to the card in HTML
+            cardBody.append(title, wind, humid); //Attach any other specifications of variables are created above
+            card.append(cardBody);
+            $("#today").append(card);
         })
     }
 
